14c96da4fd0ad4e1e43145c0e90db2055bb1b5b2
Ingrid Gabriel <jacquelinegabrielz@outlook.com>
2024-02-21 10:40:14 -0600

    creacion de entidad Cursos

---
+++ PruebaTec02IJGZ/Models/Cursos.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PruebaTec02IJGZ.Models
+{
+    public class Cursos
+    {
+        // Definición de la entidad Carrera
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+
+        [DataType(DataType.Date)]
+        public DateTime FechaInicio { get; set; }
+
+       
+    }
+}
