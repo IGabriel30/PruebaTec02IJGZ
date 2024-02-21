706d4cfb6bc543d5606dd0112b3fdb053a584255
Ingrid Gabriel <jacquelinegabrielz@outlook.com>
2024-02-21 10:53:59 -0600

    Controladores y vistas

---
+++ PruebaTec02IJGZ/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PruebaTec02IJGZ.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+        }
+        public DbSet<Cursos> Cursos { get; set; }
+    }
+}
